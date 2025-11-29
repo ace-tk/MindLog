f5d83fc16f1da37900787ccd191e1f1cc55b4471
Shriti Negi <shritinegi24@gmail.com>
2025-11-29 11:37:08 +0530

    updated journal

--- frontend/mindLog/src/context/JournalContext.js
+++ frontend/mindLog/src/context/JournalContext.js
@@ -1,22 +1,38 @@
 import React, { createContext, useState, useEffect } from "react";
 import uuid from "react-native-uuid";
+import { getEntries, addEntry as storageAddEntry } from "../services/storage";
 
 export const JournalContext = createContext();
 
 export const JournalProvider = ({ children }) => {
     const [entries, setEntries] = useState([]);
 
-    const addEntry = (entry) => {
-        setEntries((prev) => [entry, ...prev]);
-    };
+    //load data on mounting
+    useEffect(() => {
+        const loadInitialData = async () => {
+            const storedEntries = await getEntries();
+            setEntries(storedEntries);
+        };
+        loadInitialData();
+    }, [])
+ 
+
+    const addEntry = async (entry) => {
+        
+        const newEntries = await storageAddEntry(entry);
+        setEntries(newEntries.reverse())
+       
+    }
 
     const deleteEntry = (id) => {
-        setEntries((prev) => prev.filter((item) => item.id !== id));
-    };
+        
+        setEntries((prev)=>prev.filter((item)=>item.id !== id));
+    }
 
     return (
+
         <JournalContext.Provider value={{ entries, addEntry, deleteEntry }}>
             {children}
         </JournalContext.Provider>
-    );
-};
+    )
+}
