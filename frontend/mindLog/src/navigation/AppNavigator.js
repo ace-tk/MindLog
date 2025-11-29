279a5738cc5bf624d903694f4ceab804d806cfb7
Shriti Negi <shritinegi24@gmail.com>
2025-11-29 11:40:14 +0530

    added statsScreen

--- frontend/mindLog/src/navigation/AppNavigator.js
+++ frontend/mindLog/src/navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
 import JournalEntryScreen from "../screens/JournalEntryScreen";
 import MoodDetailScreen from "../screens/MoodDetailScreen";
+import StatsScreen from "../screens/StatsScreen";
 
 const Stack = createStackNavigator();
 
@@ -12,6 +13,8 @@ export default function AppNavigator() {
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="JournalEntry" component={JournalEntryScreen} />
             <Stack.Screen name="MoodDetail" component={MoodDetailScreen} />
+            <Stack.Screen name="StatsScreen" component={StatsScreen} />
+
         </Stack.Navigator>
     );
 }
